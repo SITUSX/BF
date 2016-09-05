88c242130e135ad2f611eea9aa59af16564b5276
Armour GU <779675978@qq.com>
2016-09-05 17:29:16 +0800

    Revert "client"
    This reverts commit 2edf2c0a9d91250163272d9e92235e44d5a93f57.

--- BFClient/src/service/ExecuteService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ExecuteService extends Remote{
-
-	public String execute(String code, String param) throws RemoteException;
-}
